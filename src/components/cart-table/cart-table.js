82c6f77451b3713f044f0ace6108fe7c4ff7e1d1
Natalia B <barabash4ka@gmail.com>
2020-08-27 11:25:42 +0300

    include cart-table to redux

--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -1,46 +1,72 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './cart-table.css'
 
-const CartTable = () => {
-   return (
-      <div className='cart-table'>
-        <h2>Ваш заказ</h2>
-        <table className='table'>
-          <thead>
-             <tr>
-               <th>№</th>
-               <th>Книга</th>
-               <th>Кол-во</th>
-               <th>Цена</th>
-               <th>Действие</th>
-             </tr>
-          </thead>
-  
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Властелин Колец</td>
-              <td>2</td>
-              <td>750 P</td>
-              <td>
-                <button className='btn btn-outline-danger btn-sm float-right'>
-                  <i className='fa fa-trash-o' />
-                </button>
-                <button className='btn btn-outline-success btn-sm float-right'>
-                  <i className='fa fa-plus-circle' />
-                </button>
-                <button className='btn btn-outline-warning btn-sm float-right'>
-                  <i className='fa fa-minus-circle' />
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-  
-        <div className='total'>
-          Total: 1500P
-        </div>
-      </div>
+const CartTable = ({ items, total, onInc, onDec, onDelete }) => {
+
+  const renderItems = (item, indx) => {
+    const { id, name, count, total } = item
+    return (
+      <tr key={id}>
+        <td>{indx}</td>
+        <td>{name}</td>
+        <td>{count}</td>
+        <td>{total} P</td>
+        <td>
+          <button className='btn btn-outline-danger btn-sm float-right'
+            onClick={() => onDelete(id)}>
+            <i className='fa fa-trash-o' />
+          </button>
+          <button className='btn btn-outline-success btn-sm float-right'
+            onClick={() => onInc(id)}>
+            <i className='fa fa-plus-circle' />
+          </button>
+          <button className='btn btn-outline-warning btn-sm float-right'
+            onClick={() => onDec(id)}>
+            <i className='fa fa-minus-circle' />
+          </button>
+        </td>
+      </tr>
     )
+  }
+
+  return (
+    <div className='cart-table'>
+      <h2>Ваш заказ</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>№</th>
+            <th>Книга</th>
+            <th>Кол-во</th>
+            <th>Цена</th>
+            <th>Действие</th>
+          </tr>
+        </thead>
+
+        <tbody>{items.map(renderItems)}</tbody>
+      </table>
+
+      <div className='total'>
+        Итого: {total}P
+      </div>
+    </div>
+  )
 }
-export default CartTable
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.cartItems,
+    total: state.orderTotal
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    onInc: () => console.log('onInc'), 
+    onDec: () => console.log('onDec'), 
+    onDelete: () => console.log('onDelete')
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartTable)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,20 @@
 const initialState = {
    books: [],
+   cartItems: [
+      {
+         id: '1', 
+         name: 'First', 
+         count: '2', 
+         total: 200
+      },
+      {
+         id: '2', 
+         name: 'Second', 
+         count: '5', 
+         total: 1500
+      }
+   ],
+   orderTotal: 2569,
    loading: true,
    error: null
 }
@@ -8,18 +23,21 @@ const reducer = ( state = initialState, action ) => {
    switch (action.type) {
       case 'FETCH_BOOKS_REQUEST':
          return {
+            ...state,
             books: [],
             loading: true,
             error: null
          }
       case 'FETCH_BOOKS_SUCCESS':
          return {
+            ...state,
             books: action.payload,
             loading: false,
             error: null
          }
       case 'FETCH_BOOKS_FAILURE':
          return {
+            ...state,
             books: [],
             loading: false,
             error: action.payload
